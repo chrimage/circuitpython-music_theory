1516e8043d51dd78f03025dbbcd8641c25d15ed2
Chris Smith <john.chris.smith@gmail.com>
2022-12-28 15:15:14 -0600

    Improve example code to handle NoteOn and NoteOff messages correctly

--- examples/example.py
+++ examples/example.py
@@ -9,32 +9,16 @@ print('Default output channel:', (midi.out_channel + 1))
 print('Listening on input channels:', tuple(((c + 1) for c in midi.in_channel)))
 
 while True:
-    while True:
-        msg_in = midi.receive()
-        if (isinstance(msg_in, NoteOn) and (msg_in.velocity != 0)):
-            # Convert the root note number to a Note object
-            root_note = Note.from_number(msg_in.note)
-            # Build a major chord with the root note as the tonic
-            chord = Chord.build_chord(root_note, 'I', 'major')
-            print('Playing major chord with root', root_note, 'from channel', (msg_in.channel + 1))
-            # Iterate over the notes in the chord
-            for note in chord.notes:
-                # Convert the note back to a number
-                note_number = note.to_number()
-                # Send a NoteOn message for the note
-                midi.send(NoteOn(note_number, msg_in.velocity))
-        elif (isinstance(msg_in, NoteOff) or (isinstance(msg_in, NoteOn) and (msg_in.velocity == 0))):
-            # Convert the root note number to a Note object
-            root_note = Note.from_number(msg_in.note)
-            # Build a major chord with the root note as the tonic
-            chord = Chord.build_chord(root_note, 'I', 'major')
-            # Iterate over the notes in the chord
-            for note in chord.notes:
-                # Convert the note back to a number
-                note_number = note.to_number()
-                # Send a NoteOff message for the note
-                midi.send(NoteOff(note_number, 0))
-        elif isinstance(msg_in, MIDIUnknownEvent):
-            print('Unknown MIDI event status ', msg_in.status)
-        elif (msg_in is not None):
-            midi.send(msg_in)
+    msg_in = midi.receive()
+    if isinstance(msg_in, adafruit_midi.NoteOn) and msg_in.velocity != 0:
+        # Convert the root note number to a Note object
+        root_note = Note.from_number(msg_in.note)
+        # Build a major chord with the root note as the tonic
+        chord = Chord.build_chord(root_note, 'I', 'major')
+        print('Playing major chord with root', root_note, 'from channel', (msg_in.channel + 1))
+        # Iterate over the notes in the chord
+        for note in chord.notes:
+            # Convert the note back to a number
+            note_number = note.to_number()
+            # Send a NoteOn message for the note
+            midi.send(adafruit_midi.NoteOn(note_number, msg_in.velocity))
